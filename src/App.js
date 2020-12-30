d303804192f5040bb67a6a32affdbc60f4581fee
mbrandao-tokstok <marcus.brandao@tksolution.com.br>
2020-12-29 22:56:34 -0300

    Desafio concluido

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from "./services/api"
 
 import "./styles.css";
 
 function App() {
+
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get("/repositories").then(response => {
+      setRepositories(response.data);
+    })
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post("/repositories", 
+      {
+        title : `gostack-template-conceitos ${Date.now()}`, 
+        url : "rocketseat-education/gostack-template-conceitos-nodejs", 	
+        techs : [
+          "ReactJS",
+          "NodeJS",
+          ".NET"
+        ]
+      }
+    )
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const response = await api.delete(`/repositories/${id}`);
+
+    const index = repositories.findIndex(repo => repo.id === id);
+
+    console.log(index);
+
+    repositories.splice(index, 1);
+
+    console.log(repositories);
+
+    setRepositories([...repositories]);
+
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        { repositories.map(repo => 
+          <li key={repo.id}>{repo.title} 
+            <button onClick={() => handleRemoveRepository(repo.id)}>
+              Remover
+            </button>
+          </li>) }
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
